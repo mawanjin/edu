edc5940fabf91e6a0fe30479488da55624a23585
wanjinma <13774266242@hotmail.com>
2015-08-06 01:04:59 +0800

    gps

--- src/main/java/com/thinkgem/jeesite/modules/edu/service/GpsService.java
+++ src/main/java/com/thinkgem/jeesite/modules/edu/service/GpsService.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 /**
  * gpsService
+ *
  * @author lala
  * @version 2015-04-12
  */
@@ -29,40 +30,54 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class GpsService extends BaseService {
 
-	@Autowired
-	private GpsDao gpsDao;
-	
-	public Gps get(String id) {
-		return gpsDao.get(id);
-	}
-	
-	public Page<Gps> find(Page<Gps> page, Gps gps) {
-		DetachedCriteria dc = gpsDao.createDetachedCriteria();
-		if (gps.getUser()!=null&& org.apache.commons.lang3.StringUtils.isNotEmpty(gps.getUser().getName())){
-			dc.add(Restrictions.like("user.name", "%"+gps.getUser().getName()+"%"));
-		}
-		dc.add(Restrictions.eq(Gps.FIELD_DEL_FLAG, Gps.DEL_FLAG_NORMAL));
-		dc.addOrder(Order.desc("createDate"));
-		return gpsDao.find(page, dc);
-	}
-	
-	@Transactional(readOnly = false)
-	public void save(Gps gps) {
-		gpsDao.clear();
-		gpsDao.save(gps);
-	}
-	
-	@Transactional(readOnly = false)
-	public void delete(String id) {
-		gpsDao.deleteById(id);
-	}
+    @Autowired
+    private GpsDao gpsDao;
 
-	public List<Gps> findAll(String uid) {
+    public Gps get(String id) {
+        return gpsDao.get(id);
+    }
+
+    public Page<Gps> find(Page<Gps> page, Gps gps) {
+        DetachedCriteria dc = gpsDao.createDetachedCriteria();
+        if (gps.getUser() != null && org.apache.commons.lang3.StringUtils.isNotEmpty(gps.getUser().getName())) {
+            dc.add(Restrictions.like("user.name", "%" + gps.getUser().getName() + "%"));
+        }
+        dc.add(Restrictions.eq(Gps.FIELD_DEL_FLAG, Gps.DEL_FLAG_NORMAL));
+        dc.addOrder(Order.desc("createDate"));
+        return gpsDao.find(page, dc);
+    }
+
+    public Page<Gps> find(Page<Gps> page, Gps gps, boolean front) {
+        DetachedCriteria dc = gpsDao.createDetachedCriteria();
+        if (front) {
+            dc.add(Restrictions.eq("user.id", gps.getUser().getId()));
+        } else {
+            if (gps.getUser() != null && org.apache.commons.lang3.StringUtils.isNotEmpty(gps.getUser().getName())) {
+                dc.add(Restrictions.like("user.name", "%" + gps.getUser().getName() + "%"));
+            }
+        }
+        dc.add(Restrictions.eq(Gps.FIELD_DEL_FLAG, Gps.DEL_FLAG_NORMAL));
+        dc.addOrder(Order.desc("createDate"));
+        return gpsDao.find(page, dc);
+    }
+
+    @Transactional(readOnly = false)
+    public void save(Gps gps) {
+        gpsDao.clear();
+        gpsDao.save(gps);
+    }
+
+    @Transactional(readOnly = false)
+    public void delete(String id) {
+        gpsDao.deleteById(id);
+    }
+
+    public List<Gps> findAll(String uid) {
         Gps gps = new Gps();
         Euser user = new Euser();
         user.setId(uid);
         gps.setUser(user);
-        return find(new Page<Gps>(1, 50), gps).getList();
+        return find(new Page<Gps>(1, 50), gps, true).getList();
 //		return gpsDao.find("from Gps where user.id=:p1 and del_flag =0 ",new Parameter(uid));
-	}
+    }
 }
